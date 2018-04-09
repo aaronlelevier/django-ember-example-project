9e9e00ed5e840c210d5575a71d84133c432a651f
aaronlelevier <pyaaron@gmail.com>
2018-04-09 16:32:13 -0700

    Grid with sorting and re-firing requests working

--- Rover-Ember/rover/app/components/sitter-table.js
+++ Rover-Ember/rover/app/components/sitter-table.js
@@ -1,14 +1,42 @@
 import Component from '@ember/component';
-import { computed } from "@ember/object";
-
+import { computed } from '@ember/object';
+import { isEmpty } from '@ember/utils';
+import { inject as service } from '@ember/service';
 import Table from 'ember-light-table';
+import { task } from 'ember-concurrency';
+import { get, set } from '@ember/object';
 
 export default Component.extend({
+  store: service(),
+  page: 1,
+  dir: null,
+  sort: null,
+  canLoadMore: true,
+  enableSync: true,
+  meta: null,
+  table: null,
+
+  init() {
+    this._super(...arguments);
+
+    let table = new Table(this.get('columns'), this.get('model'), { enableSync: this.get('enableSync') });
+
+    this.set('table', table);
+    this.get('fetchRecords').perform();
+  },
+  fetchRecords: task(function*() {
+    let records = yield this.get('store').query('sitter', this.getQueryParams());
+
+    this.get('model').pushObjects(records.toArray());
+    this.set('meta', records.get('meta'));
+    this.set('canLoadMore', !isEmpty(records));
+  }).restartable(),
   columns: computed(function() {
     return [{
       label: 'Photo',
       valuePath: 'photo',
-      cellComponent: 'user-avatar'
+      cellComponent: 'user-avatar',
+      sortable: false
     },{
       label: 'Name',
       valuePath: 'name',
@@ -18,8 +46,36 @@ export default Component.extend({
       cellComponent: 'ratings-score'
     }];
   }),
-
-  table: computed('model', function() {
-    return new Table(this.get('columns'), this.get('model'));
-  })
+  getQueryParams() {
+    let params = this.getProperties(['page']);
+    let orderingParam = this.getOrderingParam();
+    if (orderingParam) {
+      params['ordering'] = orderingParam;
+    }
+    return params;
+  },
+  getOrderingParam() {
+    if (this.get('dir') || this.get('sort')) {
+      let s = '';
+      if (this.get('dir') == 'desc') {
+        s += '-';
+      }
+      s += this.get('sort');
+      return s;
+    }
+  },
+  actions: {
+    onColumnClick(column) {
+      if (column.sorted) {
+        this.setProperties({
+          dir: column.ascending ? 'asc' : 'desc',
+          sort: column.get('valuePath'),
+          canLoadMore: true,
+          page: 1
+        });
+        this.get('model').clear();
+        this.get('fetchRecords').perform();
+      }
+    }
+  }
 });
